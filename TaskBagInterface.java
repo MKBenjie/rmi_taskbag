1dbccd797bdb7fa81a987656f1bc8b953c54c83a
MKBenjie <mugaggabenjie@gmail.com>
2024-02-28 19:57:07 +0300

    Interface for taskbag

---
+++ TaskBagInterface.java
@@ -0,0 +1,13 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+
+public interface TaskBagInterface extends Remote{
+
+    public void pairOut(int key, int[] value) throws RemoteException;
+    public void pairOut(String key, int[] value) throws RemoteException;
+    public void pairOut(String key, int id) throws RemoteException;
+    public int[] pairIn(int id) throws RemoteException;
+    public int pairIn(String key) throws RemoteException;
+    public List<Integer> readPair(String key) throws RemoteException;
+}
